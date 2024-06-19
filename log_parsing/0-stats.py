8904fafd5f55b61c943cb5081de4e4e1a1615cfb
Holbiwan <sabrinapapeau@gmail.com>
2024-06-19 23:16:58 +0200

    Fix print stats

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,20 +1,9 @@
 #!/usr/bin/python3
+"""
+Module parses and prints stats to stdout
+"""
+from sys import stdin
 
-"""Script that reads stdin line by line and computes metrics"""
-
-import sys
-import re
-
-
-def print_metrics(status_codes, file_size):
-    """Prints file size and status codes sorted by code."""
-    print("File size: {:d}".format(file_size))
-    for code in sorted(status_codes.keys()):
-        if status_codes[code] != 0:
-            print("{}: {:d}".format(code, status_codes[code]))
-
-
-# Initialize all required variables and regex pattern
 status_codes = {
     "200": 0,
     "301": 0,
@@ -25,26 +14,34 @@ status_codes = {
     "405": 0,
     "500": 0
 }
-file_size = 0
-counter = 0
-pattern = re.compile(
-    r'\d+\.\d+\.\d+\.\d+ - \[.*\] "GET /projects/260 HTTP/1\.1" (\d+) (\d+)')
 
-try:
-    for line in sys.stdin:
-        match = pattern.search(line)
-        if match:
-            code, size = match.groups()
-            file_size += int(size)
-            if code in status_codes:
-                status_codes[code] += 1
-            counter += 1
-            if counter == 10:
-                print_metrics(status_codes, file_size)
-                counter = 0  # Reset counter, keep file size and status codes
-except KeyboardInterrupt:
-    print_metrics(status_codes, file_size)
-    raise  # Re-raise the KeyboardInterrupt to signal interruption
+size = 0
 
-# Print final metrics
-print_metrics(status_codes, file_size)
+
+def print_stats():
+    """Prints the accumulated logs"""
+    print("File size: {}".format(size))
+    for status in sorted(status_codes.keys()):
+        if status_codes[status]:
+            print("{}: {}".format(status, status_codes[status]))
+
+
+if __name__ == "__main__":
+    count = 0
+    try:
+        for line in stdin:
+            try:
+                items = line.split()
+                size += int(items[-1])
+                if items[-2] in status_codes:
+                    status_codes[items[-2]] += 1
+            except:
+                pass
+            if count == 9:
+                print_stats()
+                count = -1
+            count += 1
+    except KeyboardInterrupt:
+        print_stats()
+        raise
+    print_stats()
