da52d806b00122f342c6fbfe1461dcf8f6264341
Holbiwan <sabrinapapeau@gmail.com>
2024-07-15 11:07:42 +0200

    Update SLIDE_LEFT direction

--- slide_line/0-slide_line.c
+++ slide_line/0-slide_line.c
@@ -1,47 +1,53 @@
 #include "slide_line.h"
 
+/**
+ * slide_line - slides and merges an array of integers
+ * @line: points to an array of integers containing size elements
+ * @size: the number of elements in the array
+ * @direction: the direction to slide and merge (SLIDE_LEFT, SLIDE_RIGHT)
+ *
+ * Return: 1 upon success, or 0 upon failure
+ */
 int slide_line(int *line, size_t size, int direction) {
     if (direction != SLIDE_LEFT && direction != SLIDE_RIGHT) {
-        return 0;
+        return 0; // Validate direction
     }
-
-    size_t i, j;  // Modifier ici pour utiliser size_t au lieu de int
 
     if (direction == SLIDE_LEFT) {
+        size_t i = 0, j = 0;
+
+        // Move non-zero elements to the front
         for (i = 0; i < size; i++) {
             if (line[i] != 0) {
-                j = i;
-                // Trouver la position j pour placer line[i]
-                while (j > 0 && line[j - 1] == 0) {
-                    j--;
-                }
-                if (j > 0 && line[j - 1] == line[i]) {
-                    line[j - 1] *= 2;
-                    line[i] = 0;
-                } else if (i != j) {
+                if (j != i) {
                     line[j] = line[i];
                     line[i] = 0;
                 }
+                j++;
             }
         }
-    } else {  // SLIDE_RIGHT
-        for (i = size - 1; i != (size_t)-1; i--) {
+
+        // Merge consecutive equal elements
+        for (i = 0; i < size - 1; i++) {
+            if (line[i] != 0 && line[i] == line[i + 1]) {
+                line[i] *= 2;
+                line[i + 1] = 0;
+                i++; // Skip the next element since it's already merged
+            }
+        }
+
+        // Move non-zero elements to the front again after merging
+        j = 0;
+        for (i = 0; i < size; i++) {
             if (line[i] != 0) {
-                j = i;
-                // Trouver la position j pour placer line[i]
-                while (j < size - 1 && line[j + 1] == 0) {
-                    j++;
-                }
-                if (j < size - 1 && line[j + 1] == line[i]) {
-                    line[j + 1] *= 2;
-                    line[i] = 0;
-                } else if (i != j) {
+                if (j != i) {
                     line[j] = line[i];
                     line[i] = 0;
                 }
+                j++;
             }
         }
     }
 
-    return 1; // Return success
+    return 1; // Success
 }
