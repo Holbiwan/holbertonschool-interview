d246e1dd8f0b0b1d2321ab11c893ac3c1383b6bd
Holbiwan <sabrinapapeau@gmail.com>
2024-07-15 11:19:32 +0200

    Try an another struct of code

--- slide_line/0-slide_line.c
+++ slide_line/0-slide_line.c
@@ -1,53 +1,101 @@
 #include "slide_line.h"
+/**
+ * reverse - reverse the array
+ * @line: pointer to the array
+ * @size: size of the array
+ * Return: Nothing.
+ **/
+void reverse(int *line, size_t size)
+{
+	size_t a, b;
+
+	a = 0;
+	b = size - 1;
+	while (a < b)
+	{
+		int temp = line[a];
+
+		line[a] = line[b];
+		line[b] = temp;
+		a++;
+		b--;
+	}
+}
 
 /**
- * slide_line - slides and merges an array of integers
- * @line: points to an array of integers containing size elements
- * @size: the number of elements in the array
- * @direction: the direction to slide and merge (SLIDE_LEFT, SLIDE_RIGHT)
- *
- * Return: 1 upon success, or 0 upon failure
+ * slide_zero - moves zeros
+ * @line: pointer to the array
+ * @size: size of the array
+ * Return: Nothing.
+ **/
+void slide_zero(int *line, size_t size)
+{
+	size_t i;
+	size_t zero = 0;
+
+	i = 0;
+	while (i < size)
+	{
+		if (line[i] == 0)
+			zero++;
+		else if (zero)
+			line[i - zero] = line[i];
+		i++;
+	}
+	while (zero > 0)
+	{
+		line[i - 1] = 0;
+		zero--;
+		i--;
+	}
+}
+
+/**
+ * slide_non_zero - moves non zeros
+ * @line: pointer to the array
+ * @size: size of the array
+ * Return: Nothing.
+ **/
+void slide_non_zero(int *line, size_t size)
+{
+	size_t i;
+
+	i = 0;
+	while (i < size)
+	{
+		if (line[i] == line[i + 1])
+		{
+			line[i] *= 2;
+			line[i + 1] = 0;
+		}
+		i += 2;
+	}
+}
+/**
+ * slide_line - function that slides and merges an array of integers.
+ * @line: points to an array of integers containing
+ * @size: elements, that must be slided & merged to the
+ * @direction: represented by direction
+ * Return: 1 upon success, or 0 upon failure.
  */
-int slide_line(int *line, size_t size, int direction) {
-    if (direction != SLIDE_LEFT && direction != SLIDE_RIGHT) {
-        return 0; /* Validate direction */
-    }
+int slide_line(int *line, size_t size, int direction)
+{
 
-    if (direction == SLIDE_LEFT) {
-        size_t i = 0, j = 0;
-
-        /* Move non-zero elements to the front */
-        for (i = 0; i < size; i++) {
-            if (line[i] != 0) {
-                if (j != i) {
-                    line[j] = line[i];
-                    line[i] = 0;
-                }
-                j++;
-            }
-        }
-
-        /* Merge consecutive equal elements */
-        for (i = 0; i < size - 1; i++) {
-            if (line[i] != 0 && line[i] == line[i + 1]) {
-                line[i] *= 2;
-                line[i + 1] = 0;
-                i++; /* Skip the next element since it's already merged */
-            }
-        }
-
-        /* Move non-zero elements to the front again after merging */
-        j = 0;
-        for (i = 0; i < size; i++) {
-            if (line[i] != 0) {
-                if (j != i) {
-                    line[j] = line[i];
-                    line[i] = 0;
-                }
-                j++;
-            }
-        }
-    }
-
-    return 1; /* Success */
+	if (direction == SLIDE_LEFT)
+	{
+		slide_zero(line, size);
+		slide_non_zero(line, size);
+		slide_zero(line, size);
+		return (1);
+	}
+	else if (direction == SLIDE_RIGHT)
+	{
+		reverse(line, size);
+		slide_zero(line, size);
+		slide_non_zero(line, size);
+		slide_zero(line, size);
+		reverse(line, size);
+		return (1);
+	}
+	return (0);
 }
