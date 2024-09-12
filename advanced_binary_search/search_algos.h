6ed259d0254a7887b460f0c7c58d84d735265dc6
Holbiwan <sabrinapapeau@gmail.com>
2024-09-12 02:50:38 +0200

    Fix patch

--- advanced_binary_search/search_algos.h
+++ advanced_binary_search/search_algos.h
@@ -4,5 +4,7 @@
 #include <stdio.h>
 
 int advanced_binary(int *array, size_t size, int value);
+int recursive_search(int *array, size_t first, size_t last, int value);
+
 
 #endif /* SEARCH_ALGOS_H */
