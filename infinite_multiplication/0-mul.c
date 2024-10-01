e01fbdf45da86a9bd7e18b312e18a57ff76e578c
Holbiwan <sabrinapapeau@gmail.com>
2024-10-02 01:54:19 +0200

    fix maalloc

--- infinite_multiplication/0-mul.c
+++ infinite_multiplication/0-mul.c
@@ -2,91 +2,86 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
+
 /**
- * _isnumber - checks if string is number
- *
- * @s: string
- *
- * Return: 1 if number, 0 if not
+ * _isnumber - checks if a string is a number
+ * @s: string to check
+ * Return: 1 if the string is a number, 0 otherwise
  */
 int _isnumber(char *s)
 {
-	int i, check, d;
+    int i;
 
-	d = 0, check = 1;
-	for (i = 0; *(s + i) != 0; i++)
-	{
-		d = isdigit(*(s + i));
-		if (d == 0)
-		{
-			check = 0;
-			break;
-		}
-	}
-	return (check);
+    for (i = 0; s[i]; i++)
+    {
+        if (!isdigit(s[i]))
+            return (0);
+    }
+    return (1);
 }
 
 /**
- * _callocX - reserves memory initialized to 0
- *
- * @nmemb: # of bytes
- *
- * Return: pointer
+ * _callocX - allocates memory and initializes it to '0'
+ * @nmemb: number of bytes to allocate
+ * Return: pointer to the allocated memory
  */
 char *_callocX(unsigned int nmemb)
 {
-	unsigned int i;
-	char *p;
+    char *p;
+    unsigned int i;
 
-	p = malloc(nmemb + 1);
-	if (p == 0)
-		return (0);
-	for (i = 0; i < nmemb; i++)
-		p[i] = '0';
-	p[i] = '\0';
-	return (p);
+    p = malloc(nmemb + 1);
+    if (p == NULL)
+        return (NULL);
+    
+    for (i = 0; i < nmemb; i++)
+        p[i] = '0';
+    p[i] = '\0';  // Null-terminate the string
+    return (p);
 }
 
 /**
- * main - multiplies inf numbers
- *
- * @argc: # of cmd line args
- * @argv: cmd line args
- * Return: No return
+ * main - multiplies two large positive numbers
+ * @argc: number of command line arguments
+ * @argv: array of command line argument strings
+ * Return: 0 on success, 98 on error
  */
 int main(int argc, char **argv)
 {
-	int i, j, l1, l2, lful, mul, add, ten, ten2, tl, zer = 0;
-	char *res;
+    int i, j, l1, l2, lful, mul, add, ten, ten2, tl, zer = 0;
+    char *res;
 
-	if (argc != 3 || _isnumber(argv[1]) == 0 || _isnumber(argv[2]) == 0)
-		printf("Error\n"), exit(98);
-	if (atoi(argv[1]) == 0 || atoi(argv[2]) == 0)
-		printf("0\n"), exit(0);
-	l1 = strlen(argv[1]), l2 = strlen(argv[2]);
-	lful = l1 + l2;
-	res = _callocX(lful);
-	if (res == 0)
-		printf("Error\n"), exit(98);
-	for (i = l2 - 1; i >= 0; i--)
-	{
-		ten = 0, ten2 = 0;
-		for (j = l1 - 1; j >= 0; j--)
-		{
-			tl = i + j + 1;
-			mul = (argv[1][j] - '0') * (argv[2][i] - '0') + ten;
-			ten = mul / 10;
-			add = (res[tl] - '0') + (mul % 10) + ten2;
-			ten2 = add / 10;
-			res[tl] = (add % 10) + '0';
-		}
-		res[tl - 1] = (ten + ten2) + '0';
-	}
-	if (res[0] == '0')
-		zer = 1;
-	for (; zer < lful; zer++)
-		printf("%c", res[zer]);
-	printf("\n");
-	free(res);
-	return (0);
-}
+    if (argc != 3 || !_isnumber(argv[1]) || !_isnumber(argv[2]))
+    {
+        printf("Error\n");
+        exit(98);
+    }
+
+    // Check if either number is zero
+    if (strcmp(argv[1], "0") == 0 || strcmp(argv[2], "0") == 0)
+    {
+        printf("0\n");
+        return (0);
+    }
+
+    l1 = strlen(argv[1]);
+    l2 = strlen(argv[2]);
+    lful = l1 + l2;
+
+    res = _callocX(lful);
+    if (res == NULL)
+    {
+        printf("Error\n");
+        exit(98);
+    }
+
+    for (i = l2 - 1; i >= 0; i--)
+    {
+        ten = 0;
+        ten2 = 0;
+        for (j = l1 - 1; j >= 0; j--)
+        {
+            tl = i + j + 1;
+            mul = (argv[1][j] - '0') * (argv[2][i] - '0') + ten;
+            ten = mul / 10;
+            add = (res[tl] - '
