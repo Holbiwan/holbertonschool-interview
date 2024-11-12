47c725730c54fd6e5e14ff41559a1db67e83223a
Holbiwan <sabrinapapeau@gmail.com>
2024-11-12 19:16:18 +0100

    Fix PEP 8

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python3
-"""Returns the perimeter of the island."""
+"""
+Function returns perimeter of the island
+"""
+
 
 def island_perimeter(grid):
-    """Calculate the perimeter of an island in a grid."""
-    count = 0
-    row = len(grid)
-    col = len(grid[0]) if row else 0
+    """[summary]
 
-    for i in range(row):
-        for j in range(col):
-            if grid[i][j]:
-                # Define neighboring indices
-                idx = [(i - 1, j), (i, j - 1), (i, j + 1), (i + 1, j)]
-                # Check if each neighbor is within bounds
-                check = [
-                    1 if k[0] in range(row) and k[1] in range(col) else 0
-                    for k in idx
-                ]
-                # Add to count if a neighbor is water or out of bounds
-                count += sum(
-                    1 if not r or not grid[k[0]][k[1]] else 0
-                    for r, k in zip(check, idx)
-                )
+    Args:
+        grid ([type]): [description]
 
-    return count
+    Returns:
+        [type]: [description]
+    """
+
+    perimeter = 0
+    m = len(grid)
+    n = len(grid[0])
+
+    for i in range(m):
+        for j in range(n):
+            if grid[i][j] == 1:
+                for x, y in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
+                    α, β = i + x, j + y
+                    # print(α, β)
+                    if α >= m or β >= n or α < 0 or β < 0 or grid[α][β] == 0:
+                        perimeter += 1
+
+    return perimeter
