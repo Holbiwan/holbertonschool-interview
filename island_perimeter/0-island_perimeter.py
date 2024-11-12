4ca93e4b8a29b9917029d2209eef49b3112eb7c7
Holbiwan <sabrinapapeau@gmail.com>
2024-11-12 19:07:13 +0100

    Fix syntax of code

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -1,26 +1,33 @@
 #!/usr/bin/python3
-
-""" Returns the perimeter of the island"""
+"""Returns the perimeter of the island."""
 
 
 def island_perimeter(grid):
     """
-    Input: List of Lists
-    Returns: Perimeter of the island
+Calculate the perimeter of an island in a grid.
+    Args:
+    grid (list of list of int): The grid representing the island.
+Returns:
+        int: The perimeter of the island.
     """
     count = 0
     row = len(grid)
     col = len(grid[0]) if row else 0
 
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-
-            idx = [(i - 1, j), (i, j - 1), (i, j + 1), (i + 1, j)]
-            check = [1 if k[0] in range(row) and k[1] in range(col) else 0
-                     for k in idx]
-
+    for i in range(row):
+        for j in range(col):
             if grid[i][j]:
-                count += sum([1 if not r or not grid[k[0]][k[1]] else 0
-                              for r, k in zip(check, idx)])
+                # Define neighboring indices
+                idx = [(i - 1, j), (i, j - 1), (i, j + 1), (i + 1, j)]
+                # Check if each neighbor is within bounds
+                check = [
+                    1 if k[0] in range(row) and k[1] in range(col) else 0
+                    for k in idx
+                ]
+                # Add to count if a neighbor is water or out of bounds
+                count += sum(
+                    1 if not r or not grid[k[0]][k[1]] else 0
+                    for r, k in zip(check, idx)
+                )
 
-    return (count)
+    return count
