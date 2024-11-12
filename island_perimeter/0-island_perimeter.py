a7255b2a549ad87cd3156adff947800b93c8439e
Holbiwan <sabrinapapeau@gmail.com>
2024-11-12 20:56:03 +0100

    Fix docstring issues

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 """
-Module used to
+Module that provides the island_perimeter function.
 """
 
 
 def island_perimeter(grid):
-    """[summary]
+    """Calculate the perimeter of an island represented in a grid.
 
     Args:
-        grid ([type]): [description]
+        grid (list of list of int): A grid where 0 represents water and 1
+            represents land.
 
     Returns:
-        [type]: [description]
+        int: The perimeter of the island.
     """
-
     perimeter = 0
     m = len(grid)
     n = len(grid[0])
@@ -21,10 +21,10 @@ def island_perimeter(grid):
     for i in range(m):
         for j in range(n):
             if grid[i][j] == 1:
-                for x, y in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
-                    α, β = i + x, j + y
-                    # print(α, β)
-                    if α >= m or β >= n or α < 0 or β < 0 or grid[α][β] == 0:
+                for dx, dy in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
+                    new_i, new_j = i + dx, j + dy
+                    if (new_i >= m or new_j >= n or new_i < 0 or new_j < 0 or
+                            grid[new_i][new_j] == 0):
                         perimeter += 1
 
     return perimeter
