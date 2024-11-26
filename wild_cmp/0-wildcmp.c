96572acfe00f420b10f62be51b63eb92eefb7962
Holbiwan <sabrinapapeau@gmail.com>
2024-11-26 03:38:54 +0100

    Remove comments and fix betty style

--- wild_cmp/0-wildcmp.c
+++ wild_cmp/0-wildcmp.c
@@ -11,21 +11,16 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-    // If both strings are empty, they are identical.
     if (*s1 == '\0' && *s2 == '\0')
         return (1);
 
-    // If the characters match, move to the next characters in both strings.
     if (*s1 == *s2)
         return (wildcmp(s1 + 1, s2 + 1));
 
-    // If `*` is encountered in s2.
     if (*s2 == '*')
     {
-        // Consider `*` as an empty string OR match it with a character in s1.
         return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
     }
 
-    // Otherwise, the strings are not identical.
     return (0);
 }
